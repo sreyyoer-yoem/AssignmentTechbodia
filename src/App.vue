e598dfea9e138ef991006ecf85b4129544fa7ffc
sreyyoer-yoem <sreyyoeryoem@gmail.com>
2023-11-12 10:28:48 +0700

    uplode folder project to GigHub

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-center text-2xl mt-10"><strong>List of Contry</strong></h1>
+    <CountryCatalog />
+  </div>
+</template>
+
+<script>
+import CountryCatalog from './components/CountryCatalog.vue';
+
+export default {
+  components: {
+    CountryCatalog
+  }
+};
+</script>
